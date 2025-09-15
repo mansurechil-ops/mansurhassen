b91f4f99dbe31ca4dc17815d2b64692937d6e2d8
mansurechil-ops <mnsurechil@gmail.com>
2025-09-15 18:08:45 +0200

    d

---
+++ tema03/eksempel2.php
@@ -0,0 +1,13 @@
+<?php     /* Eksempel 2 */
+/*
+/*    Programmet skriver ut tallene fra 1 til 10 p� hver sin linje
+/*    Eksempelet viser bruk av while-setning
+*/
+  $tall=1;  
+
+  while ($tall<=10)
+    {
+      print("$tall <br/>"); 
+      $tall++;  
+    }
+?>
