c330c0950a07ba719985351f90c08a7fb50a6004
mansurechil-ops <mnsurechil@gmail.com>
2025-10-20 18:08:14 +0200

    m

--- oblig2/index.php
+++ oblig2/index.php
@@ -1,6 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
+
+<body>
 <?php
-  include("start.html");
-  include("slutt.html");
+include("start.html");
+include("slutt.html");
+?> 
+</body>
+
+</html>
+
+
 
 
 
