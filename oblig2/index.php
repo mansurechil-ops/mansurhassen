91596e3de719d74365c0ac9de882c588587187a3
mansurechil-ops <mnsurechil@gmail.com>
2025-10-20 19:43:13 +0200

    m

--- oblig2/index.php
+++ oblig2/index.php
@@ -1,13 +1,29 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+?>
 <!DOCTYPE html>
-<html>
+<html lang="no">
 <head>
-  <title>Title of the document</title>
+  <meta charset="utf-8" />
+  <title>Obligatorisk oppgave 2</title>
 </head>
 <body>
-  <?php
-  include("start.html");
-  include("slutt.html");
-  ?>
+
+  <?php include __DIR__ . '/start.html'; ?>
+
+  <main>
+    <?php
+      // make sure these files echo the tables (and do NOT contain a full <html> document)
+      require __DIR__ . '/dbtilkobling.php';
+      include __DIR__ . '/visalleklasser.php';
+      include __DIR__ . '/visallestudenter.php';
+      // include __DIR__ . '/visstudent.php';   // if you want the single-student view too
+    ?>
+  </main>
+
+  <?php include __DIR__ . '/slutt.html'; ?>
+
 </body>
 </html>
 
