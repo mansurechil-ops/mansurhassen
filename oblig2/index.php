643e8f84187c0b3512506372ad5703ddbdccd8dc
mansurechil-ops <mnsurechil@gmail.com>
2025-10-20 18:29:19 +0200

    m

--- oblig2/index.php
+++ oblig2/index.php
@@ -1,18 +1,16 @@
-
 <!DOCTYPE html>
 <html>
 <head>
-<title>Title of the document</title>
+  <title>Title of the document</title>
 </head>
-
 <body>
-<?php
-include("start.html");
-include("slutt.html");
-?> 
+  <?php
+  include("start.html");
+  include("slutt.html");
+  ?>
 </body>
-
 </html>
+
 
 
 
