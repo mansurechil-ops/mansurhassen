aeed0651bb4b8e9d8daa98b306f88b99bc35dbec
mansurechil-ops <mnsurechil@gmail.com>
2025-10-20 17:47:43 +0200

    m

--- oblig2/index.php
+++ oblig2/index.php
@@ -1,20 +1,17 @@
-body {
-  background-color: #f0fff4; /* pale mint background */
-  font-family: "Helvetica Neue", Arial, sans-serif;
-  color: #2d3436;
-}
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+</head>
 
-h1 {
-  color: #2e7d32;           /* dark green title */
-  text-align: center;
-  margin-top: 40px;
-}
+<body>
+<?php
+include("start.html");
+include("slutt.html");
+?> 
+</body>
 
-footer {
-  color: #4caf50;
-  text-align: center;
-  margin-top: 40px;
-  font-size: 0.9em;
-}
+</html>
+
 
 
