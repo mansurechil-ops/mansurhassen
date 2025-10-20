cd19a385f94cdb6608293058b28d00ce17e431de
mansurechil-ops <mnsurechil@gmail.com>
2025-10-20 18:06:13 +0200

    m

--- oblig2/index.php
+++ oblig2/index.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Title of the document</title>
-</head>
-
-<body>
 <?php
-include("start.html");
-include("slutt.html");
-?> 
-</body>
+  include("start.html");
+  include("slutt.html");
 
-</html>
 
 
 
